041a3cfdc86ab4c5802a20cc8ca764e298d81ee8
Viralwow <132092012+Viralwow@users.noreply.github.com>
2023-09-11 05:59:54 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -90,13 +90,7 @@ const Post: React.FC<PostProps> = (props) => {
 				<meta property="og:site_name" content={host.split('.')[0]} />
 				<meta property="article:published_time" content={post.dateGmt} />
 				<meta property="article:modified_time" content={post.modifiedGmt} />
-				<meta property="og:image:width" content="1200">
-<meta property="og:image:height" content="630">
-
-				<meta
-					property="og:image:alt"
-					content={post.featuredImage.node.altText || post.title}
-				/>
+				
 				<title>{post.title}</title>
 			</Head>
 			<div className="post-container">
