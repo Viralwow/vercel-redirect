231bef8b4c67a4b4a79afd7db61d300906da4cda
Viralwow <132092012+Viralwow@users.noreply.github.com>
2023-09-23 07:47:08 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,8 +5,3 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
-module.exports = {
-  eslint: {
-    dirs: ['pages', 'utils'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
-  },
-}
