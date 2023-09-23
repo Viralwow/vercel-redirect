65bca2f445e3848ae57774330cdb4504fecbf2f8
Viralwow <132092012+Viralwow@users.noreply.github.com>
2023-09-23 07:28:55 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -82,29 +82,33 @@ const Post: React.FC<PostProps> = (props) => {
 	return (
 		<>
 			<Head>
-				<meta property="og:title" content={post.title} />
+				
 				<link rel="canonical" href={`https://${host}/${path}`} />
-				<meta property="og:description" content={removeTags(post.excerpt)} />
+			<!--
+					<meta property="og:description" content={removeTags(post.excerpt)} />
 				<meta property="og:url" content={`https://${host}/${path}`} />
 				<meta property="og:type" content="article" />
 				<meta property="og:locale" content="en_US" />
 				<meta property="og:site_name" content={host.split('.')[0]} />
 				<meta property="article:published_time" content={post.dateGmt} />
 				<meta property="article:modified_time" content={post.modifiedGmt} />
-				<meta property="og:image" content={post.featuredImage.node.sourceUrl} />
-				<meta property="og:image:width" content="1200" />
-<meta property="og:image:height" content="630" />
+				<!--<meta property="og:image:width" content="1024">
+				<meta property="og:image:height" content="614">
+				<meta property="og:image:type" content="image/png">
+		-->
+				
+				
 				<meta
 					property="og:image:alt"
-					content={post.featuredImage.node.altText || post.title}
+					content={post.featuredImage.node}
 				/>
-				<title>{post.title}</title>
+				
 			</Head>
 			<div className="post-container">
-				<h1>{post.title}</h1>
+				
 				<img
 					src={post.featuredImage.node.sourceUrl}
-					alt={post.featuredImage.node.altText || post.title}
+					alt={post.featuredImage.node.altText }
 				/>
 				<article dangerouslySetInnerHTML={{ __html: post.content }} />
 			</div>
